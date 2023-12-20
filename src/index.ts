bd60b5f95bc45aec3bd5c087cb34be59cfe3caaa
fuchs <hmcfuchs@gmail.com>
2023-12-20 13:17:07 -0300

    New Methods

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,16 @@ export const sum = (a: number, b: number) => {
   return a + b;
 };
 
+export const genRandom = (min: number, max: number): number => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export const isEven = (num: number): boolean => {
+  return num % 2 === 0;
+}
+
 module.exports = {
   sum,
+  genRandom,
+  isEven,
 }
